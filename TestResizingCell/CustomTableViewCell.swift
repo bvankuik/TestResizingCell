f062274f3e5663396d357f4405ccbc945e7f7c52
Bart van Kuik <bart@dutchvirtual.nl>
2017-01-20 14:30:16 +0100

    Basic version to demonstrate problem

---
+++ TestResizingCell/CustomTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CustomTableViewCell.swift
+//  TestResizingCell
+//
+//  Created by Bart van Kuik on 20/01/2017.
+//  Copyright © 2017 DutchVirtual. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var expandButton: UIButton!
+    
+    @IBOutlet weak var nameCorrectionTextField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        self.nameCorrectionTextField.isHidden = true
+    }
+
+    @IBAction func expandButtonTouchUpInside(_ sender: Any) {
+        self.nameCorrectionTextField.isHidden = false
+    }
+}
--- TestResizingCell/ViewController.swift
+++ TestResizingCell/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private var items: [String] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - UITableViewDataSource, UITableViewDelegate
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableViewCell") as! CustomTableViewCell
+        
+        NSLog("self.size=\(self.size)")
+        
+        cell.nameLabel.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: - View cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        items = [
+            "Allen","Upton","Hu","Yuli","Tiger","Flynn","Lev","Kyle","Sylvester","Mohammad",
+            "Harlan","Julian","Sebastian","Porter","Preston","Palmer","Jakeem","Micah","Stephen","Tucker",
+            "Abbot","Caldwell","Steven","Gannon","Kasper","Jerome","Samson","Grant","Calvin","Nero",
+            "Igor","Roth","Abbot","Devin","Geoffrey","Ryan","Tobias","Oliver","Elton","Ivan",
+            "Plato","Kevin","Dustin","Harrison","Galvin","Gannon","Reece","Price","Maxwell","Rashad",
+            "Cooper","Aristotle","Dieter","Abraham","Louis","Akeem","Gary","Kamal","Hasad","Lance",
+            "Calvin","Lester","Thaddeus","Flynn","Silas","Lev","Carl","Harper","Drake","Travis",
+            "Herrod","Austin","Howard","Martin","Oscar","Ignatius","Wing","Howard","Justin","Dexter",
+            "Tucker","Alec","Hector","Hayes","Murphy","Wallace","Brandon","Gage","Dillon","Aquila",
+            "Cullen","Ivan","Brendan","Basil","Harrison","Zeph","George","Dylan","Bernard","Gary",
+        ]
+        
+        self.tableView.estimatedRowHeight = 150
+        self.tableView.rowHeight = UITableViewAutomaticDimension
     }
-
 
 }
 
